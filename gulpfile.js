fe12c761ca286e0deb4b3645313563957f8c1f74
Jonathon Choo <jonno.choo@gmail.com>
2015-03-19 22:21:07 +1100

    Added notifications to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
-var concat = require('gulp-concat');
 var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
+var notify = require("gulp-notify");
 var sass = require('gulp-sass');
-var uglify = require('gulp-uglify');
 var watch = require('gulp-watch');
 
-gulp.task('default', ['nodemon', 'watch'])
+gulp.task('default', ['nodemon', 'sass-watch'])
 
-gulp.task('watch', function() {
+// SASS tasks
+gulp.task('sass-watch', function() {
   gulp.watch('scss/**/*.scss', ['sass']);
 });
 
@@ -17,19 +17,14 @@ gulp.task('sass', function() {
       errLogToConsole: true,
       includePaths: ['./bower_components/foundation/scss']
     }))
-    .pipe(gulp.dest('public/css/'));
+    .pipe(gulp.dest('public/css/'))
+    .pipe(notify("SASS recompiled"));
 });
 
+// gulp-nodemon
 gulp.task('nodemon', function () {
-  nodemon({ script: 'app.js', ext: 'jade js', ignore: ['gulpfile.js'] })
-    .on('restart', function () {
-      console.log('restarted!')
-    })
+  nodemon({ script: 'app.js', ext: 'jade js', ignore: ['gulpfile.js'] })    
+    .on('restart', notify.onError({
+      message: 'server has restarted'
+    }))
 })
-
-gulp.task('build', function(){
-    return gulp.src(['public/js/*.js'])
-        .pipe(concat('app.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/js'));
-});
