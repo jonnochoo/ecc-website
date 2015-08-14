a9c5cb46a8ecc122a00f2ea7a2cfc28bda13e1b5
Jonathon Choo <jonno.choo@gmail.com>
2015-08-14 22:52:21 +1000

    Refactor code

--- app.js
+++ app.js
@@ -3,18 +3,17 @@ var config = require('./config');
 var express = require('express');
 var path = require('path');
 var routes = require('./routes');
-var staticRouter = require('./routes/static');
 
 var port = process.env.PORT || 3000;
 
 var app = express();
-var viewFolder = 'views';
-app.set('views', path.join(__dirname, viewFolder));
+
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', staticRouter(viewFolder));
+
 app.use('/', routes);
 
 app.use(function(req, res){
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var eventRouter = require('./events');
 var contactRouter = require('./contact');
+var staticRouter = require('./static');
 
 var router = express.Router();
 
 router.use('/events', eventRouter);
 router.use('/contact', contactRouter);
+router.use('/', staticRouter('views'));
 
 module.exports = router;
