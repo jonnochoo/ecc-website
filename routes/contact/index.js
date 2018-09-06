44f20c2b3ca8475e1c5da768667b9488c34a6836
Jonathon Choo <jonno.choo@gmail.com>
2018-09-06 21:14:48 +1000

    Added captcha

--- routes/contact/index.js
+++ routes/contact/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var email = require('../../lib/email');
+var request = require('request');
+var config = require('../../config');
 
 var router = express.Router();
 router.get('/', getContact);
@@ -14,17 +16,26 @@ function getContact(req, res) {
   });
 }
 
-function postContact(req, res, next) {    
+function postContact(req, res, next) {
   var subject = 'Message From ECC Website: ' + req.body.name;
   var body = req.body.message;
   var from = req.body.from;
 
-  // Save to database...
-  // Keen?
-
-  email.sendAsync(subject, body, from).then(function() {
-    res.redirect('/contact');
-  }).catch(function(err) {
-    next(err);
+  var secretKey = config.express.recaptchaSecret;
+  var uri = 'https://www.google.com/recaptcha/api/siteverify?secret=' + secretKey + '&response=' + req.body['g-recaptcha-response'];
+  request.post({ uri: uri, method: 'POST' }, function (req1, res1, body1) {
+    console.log(body1)
+    var result = JSON.parse(body1);
+    if (result.success) {
+      email.sendAsync(subject, body, from).then(function () {
+        res.redirect('/contact');
+      }).catch(function (err) {
+        next(err);
+      });
+    } else {
+      next('failed captcha');
+    }
   });
+
+
 }
